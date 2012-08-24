df256725e04d130d5ee667fff8e92932aa086a62
Jerome Waldispuhl <waldispuhl@gmail.com>
2012-08-24 12:14:01 -0400

    Added ranking using data table query plugin

--- js/page/main.page.js
+++ js/page/main.page.js
@@ -1,8 +1,87 @@
 (function() {
 	$.page = {
 		ranking : function() {
-			this.protocal("content/ranking.html");
+            
+            $.ajax({
+				//url : "http://phylo.cs.mcgill.ca/phpdb/fullrankingsget.php?lang=" + window.langOpt.toUpperCase(),
+                url : "content/testranking.html",
+				type : "post",
+			}).done(function(re) {
+            
+                $("#mid-panel").html(re);
+                
+                $('.dataTable').dataTable({
+                    "sPaginationType": "full_numbers",
+                    "aaSorting": [[ 5, "desc" ]],
+                    "oLanguage": {
+                        "sUrl": "js/extra/datatable_" + window.langOpt.toUpperCase() + ".txt"
+                    },
+                    "fnDrawCallback": function ( oSettings ) {
+                        if ( oSettings.bSorted || oSettings.bFiltered ) {
+                            for ( var i=0, iLen=oSettings.aiDisplay.length ; i<iLen ; i++ ) {
+                                $('td:eq(0)', oSettings.aoData[ oSettings.aiDisplay[i] ].nTr ).html( i+1 );
+                            }
+                        }
+                    },
+                    "aoColumnDefs": [ { "bSortable": false, "aTargets": [ 0 ] } ]                    
+                });
+                
+                $(document).ready(function(){
+                    $("#showall").click(function(){
+                        $("#weekly").hide();
+                        $("#monthly").hide();
+                        $("#alltime").show();
+                        $("#showmonth").css("background-color","#fff");
+                        $("#showmonth").css("color","#000");
+                        $("#showweek").css("background-color","#fff");
+                        $("#showweek").css("color","#000");
+                        $("#showall").css("background-color","#CC0000");
+                        $("#showall").css("color","#fff");
+                    });
+                });
+
+                $(document).ready(function(){
+                    $("#showmonth").click(function(){
+                        $("#alltime").hide();
+                        $("#weekly").hide();
+                        $("#monthly").show();
+                        $("#showall").css("background-color","#fff");
+                        $("#showall").css("color","#000");
+                        $("#showweek").css("background-color","#fff");
+                        $("#showweek").css("color","#000");
+                        $("#showmonth").css("background-color","#CC0000");
+                        $("#showmonth").css("color","#fff");
+                    });
+                });
+                $(document).ready(function(){
+                    $("#showweek").click(function(){
+                        $("#alltime").hide();
+                        $("#monthly").hide();
+                        $("#weekly").show();
+                        $("#showall").css("background-color","#fff");
+                        $("#showall").css("color","#000");
+                        $("#showmonth").css("background-color","#fff");
+                        $("#showmonth").css("color","#000");
+                        $("#showweek").css("background-color","#CC0000");
+                        $("#showweek").css("color","#fff");
+                    });
+                });
+                
+                // initial display
+
+                $(document).ready(function(){
+                        $("#weekly").hide();
+                        $("#monthly").hide();
+                        $("#alltime").show();
+                        $("#showall").css("background-color","#CC0000");
+                        $("#showall").css("color","#fff");
+                });
+
+			});            
+            
 		},
+
+
 		play : function() {
 			this.protocal("content/play.html");
 		},
